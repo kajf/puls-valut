1c3e2865295b7286488a43e9bef6710d22026db7
Pavel Letsiaha <pavel_letsiaha@epam.com>
2020-05-29 19:24:27 +0300

    - places DB update - auxiliary certificate update

--- app/sql/update.bat
+++ app/sql/update.bat
@@ -1,5 +1,5 @@
-SET SQLITE_HOME=d:\opt
-SET APP_HOME=d:\_ws_a\puls-valut
+SET SQLITE_HOME=c:\opt
+SET APP_HOME=c:\_ws_a\puls-valut
 SET DB_NAME=androidovich.db
 
 %SQLITE_HOME%\sqlite3.exe -bail %APP_HOME%\auxiliary\%DB_NAME% < %APP_HOME%\app\sql\meta.sql
